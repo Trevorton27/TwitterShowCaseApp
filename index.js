4c5dd981a8be8d3cde23808d1c9af9c3460287ed
Trevor Mearns <spiral272@gmail.com>
2019-03-28 15:33:49 +0900

    struggling with tweet display funcitonality

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const app = express();
 const request = require('request-promise');
@@ -7,21 +5,22 @@ const path = require('path');
 
 
 var accessToken = '';
-var searchText = 'cats';
 
-console.log('static dir: ', path.join(__dirname, 'react', 'build', 'static'));
+
 app.use('/static', express.static(path.join(__dirname, 'react', 'build', 'static')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'react', 'build', 'index.html'));
-});
+
 
 app.get('/api/tweets', (request, response) => {
    
-    console.log('GET /api/tweets endpoint was hit')
- getTweets(accessToken).then(tweets => {
+    console.log('GET query is: ', request.query.searchTerm);
+    getTweets(accessToken, request.query.searchTerm).then(tweets => {
      response.send(JSON.parse(tweets).statuses[0]);
  });
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'react', 'build', 'index.html'));
 });
 
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
@@ -38,14 +37,14 @@ request.post('https://api.twitter.com/oauth2/token', {form: {grant_type: 'client
     })
     .auth('upS14sOp8Tk8ufcefKkm0wTVw', 'TawHbqfC2AeTp5OATi3pLpwavShfgX13yid7EDDjaOn4PKImvM', true);
 
-function getTweets(accessToken) {
+function getTweets(accessToken, searchTerm) {
     return request.get('https://api.twitter.com/1.1/search/tweets.json', 
     {
         'auth': {
             'bearer': accessToken
         },
         qs: {
-            q: searchText
+            q: searchTerm
         }
     });
 }
--- react/src/search.js
+++ react/src/search.js
@@ -3,29 +3,7 @@ import './index.css';
 import './App.css';
 import axios from 'axios';
 
-const tweetData = {
-  id_str: '',
-  user: {
-    name: '',
-    screen_name: '',
-    profile_image_url: ''
-  },
-  text: '',
-  created_at: '',
-  favorite_count: '',
-  retweet_count: '',
-  entities: {
-    media: [],
-    urls: [],
-    user_mentions: [],
-    hashtags: [],
-    symbols: []
-  } 
-}
-
-
-
-
+const app = express();
 
 
 class Search extends React.Component {
@@ -43,9 +21,17 @@ class Search extends React.Component {
   searchTweets() {
     console.log('searchTweets function was run');
     if (this.state.searchTerm !== '') {
-      this.setState({searchResults: []});
+      this.setState({
+        searchResults: []
+      });
 
-    axios.get('/api/tweets?searchTerm=cats').then((response) =>{
+    var getRequestOptions = {
+      params: {
+        searchTerm: this.state.searchTerm
+      }
+    };
+
+    axios.get('/api/tweets', getRequestOptions).then((response) =>{
       console.log(response.data);
       this.setState({searchResults: response.data});
      })
@@ -57,6 +43,16 @@ class Search extends React.Component {
       searchTerm: event.target.value
   })
 }
+
+formatTweets = (tweets) => {
+  this.setState.searchResults {
+  tweets.map(searchResults);
+  console.log(searchResults, 'formatTweets has fired');
+  }
+}
+
+
+
 
   render() {
 
@@ -81,7 +77,7 @@ class Search extends React.Component {
           <button className="btn2 btn-elegant btn-rounded btn-sm my-0" type="button" onClick={this.searchTweets}>Find Tweets</button>
         </form>
         <div className="card">
-        <span> {this.state.searchResults.tweetData} </span>
+        <span> {this.state.searchResults.formatTweets} </span>
         </div>
       </div>
     );
