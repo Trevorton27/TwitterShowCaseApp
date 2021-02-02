c288bbe2b677b9e64978d1e6b618434977765ede
Trevor Mearns <spiral272@gmail.com>
2021-02-02 12:27:42 +0900

    issues with axios

--- client/src/search.js
+++ client/src/search.js
@@ -4,110 +4,120 @@ import './App.css';
 import axios from 'axios';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 
-
 class Search extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        searchTerm: '',
-        searchResults: []
-      }
-
-      this.searchTweets = this.searchTweets.bind(this);
-    }
-
-searchTweets() {
-  console.log('searchTweets function done got run now.');
-  if (this.state.searchTerm !== '') {
-    this.setState({
+    this.state = {
+      searchTerm: '',
       searchResults: []
-    });
-
-    var getRequestOptions = {
-      params: {
-        searchTerm: this.state.searchTerm
-      }
     };
 
-    axios.get('/api/tweets', getRequestOptions).then((response) => {
-      console.log(response.data);
-      this.setState({
-        searchResults: response.data
-      });
-    })
+    this.searchTweets = this.searchTweets.bind(this);
   }
-}
 
-handleInputChange = (event) => {
-  this.setState({
-    searchTerm: event.target.value
-  })
-}
+  searchTweets() {
+    console.log('searchTweets function done got run now.');
+    if (this.state.searchTerm !== '') {
+      this.setState({
+        searchResults: []
+      });
 
-renderTweets = (tweets) => {
+      var getRequestOptions = {
+        params: {
+          searchTerm: this.state.searchTerm
+        }
+      };
 
-  console.log('Yup. renderTweets function was run, sure enough.');
-  tweets.length = 10
+      axios.get('/api/tweets', getRequestOptions).then((response) => {
+        console.log(response.data);
+        this.setState({
+          //response.statuses.data?
+          searchResults: response.statuse.data
+        });
+      });
+    }
+  }
 
-  return (tweets.map(tweet => {
-    return (
-      <div className="card">
-        <div className="tweet">
-       <img className="profile-image" src={tweet.user.profile_image_url_https} />
+  handleInputChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  };
+
+  renderTweets = (tweets) => {
+    console.log('Yup. renderTweets function was run, sure enough.');
+    tweets.length = 10;
+
+    return tweets.map((tweet) => {
+      return (
+        <div className='card'>
+          <div className='tweet'>
+            <img
+              className='profile-image'
+              src={tweet.user.profile_image_url_https}
+            />
+          </div>
+          <div className='tweet'>{tweet.user.description}</div>
+          <div className='tweet'>Name: {tweet.user.name}</div>
+          <div className='tweet'>Screen Name: {tweet.user.screen_name}</div>
+          <div className='tweet'>Text: {tweet.text}</div>
+          <div className='tweet'>Created: {tweet.created_at}</div>
         </div>
-        <div className="tweet">
-        {tweet.user.description}
-        </div>
-        <div className="tweet">
-         Name: {tweet.user.name}
-        </div>
-        <div className="tweet">
-        Screen Name: {tweet.user.screen_name}
-        </div>
-        <div className="tweet">
-        Text: {tweet.text}
-        </div>
-        <div className="tweet">
-        Created: {tweet.created_at}
-        </div>
-      </div>
-    )
-  }))
-}
+      );
+    });
+  };
 
   render() {
-
-  return (
-    <PerfectScrollbar>
-      <div className="container">
-      <br/>
-      <h3 className="card-title">Welcome To The Search Page. <br/>Enter your query below. </h3>
-            <p>
-            <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    return (
+      <PerfectScrollbar>
+        <div className='container'>
+          <br />
+          <h3 className='card-title'>
+            Welcome To The Search Page. <br />
+            Enter your query below.{' '}
+          </h3>
+          <p>
+            <button
+              className='btn btn-primary'
+              type='button'
+              data-toggle='collapse'
+              data-target='#collapseExample'
+              aria-expanded='false'
+              aria-controls='collapseExample'
+            >
               Questions?
             </button>
-            </p>
-          <div className="collapse" id="collapseExample">
-            <div className="card card-body">
-              Enter your search either by keyword or user name. This app will then search high and low throughout Twitter to bring you results from the last 7 days. 
-               Isn't that just swell?
+          </p>
+          <div className='collapse' id='collapseExample'>
+            <div className='card card-body'>
+              Enter your search either by keyword or user name. This app will
+              then search high and low throughout Twitter to bring you results
+              from the last 7 days. Isn't that just swell?
             </div>
           </div>
-        <form className="form-inline md-form mr-auto mb-4">
-              <br/>
-          <input className="form-control mr-sm-2" type="text" placeholder=" " value={this.props.value} onChange={this.handleInputChange} />
-          <button className="btn2 btn-elegant btn-rounded btn-sm my-0" type="button" onClick={this.searchTweets}>Find Tweets</button>
-        </form>
-        {this.renderTweets(this.state.searchResults)}
-      </div>
+          <form className='form-inline md-form mr-auto mb-4'>
+            <br />
+            <input
+              className='form-control mr-sm-2'
+              type='text'
+              placeholder=' '
+              value={this.props.value}
+              onChange={this.handleInputChange}
+            />
+            <button
+              className='btn2 btn-elegant btn-rounded btn-sm my-0'
+              type='button'
+              onClick={this.searchTweets}
+            >
+              Find Tweets
+            </button>
+          </form>
+          {this.renderTweets(this.state.searchResults)}
+        </div>
       </PerfectScrollbar>
     );
   }
 }
-
-
-
-
 
 export default Search;
--- index.js
+++ index.js
@@ -1,25 +1,59 @@
+require('dotenv').config;
 const express = require('express');
-const app = express();
-const request = require('request-promise');
 const path = require('path');
-
+const axios = require('axios');
+const { response } = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
+let accessToken = '';
+const getToken = () => {
+  if (accessToken) return accessToken;
+  const config = {
+    headers: {
+      Content_Type: 'application/json'
+    },
+    auth: {
+      username: process.env.API_KEY,
+      password: process.env.API_SECRET_KEY
+    }
+  };
+  return axios
+    .post(
+      'https://api.twitter.com/oauth2/token',
+      'grant_type=client_credentials',
+      config
+    )
+    .then((response) => {
+      axios.defaults.headers.common = {
+        Authorization: `Bearer ${response.data.access_token}`
+      };
+      return response.data.access_token;
+    })
+    .catch((error) => console.log(error));
+};
 
-var accessToken = '';
+getToken();
 
 app.use(
   '/static',
-  express.static(path.join(__dirname, 'react', 'build', 'static'))
+  express.static(path.join(__dirname, 'client', 'build', 'static'))
 );
-
-app.get('/api/tweets', (request, response) => {
-  console.log('GET query is: ', request.query.searchTerm);
-  getTweets(accessToken, request.query.searchTerm).then((tweets) => {
-    response.send(JSON.parse(tweets).statuses);
-  });
+app.get('/api/tweets', async (req, res) => {
+  await getToken();
+  const searchTerm = req.query.searchTerm;
+  axios
+    .get(`https://api.twitter.com/1.1/search/tweets.json?q=${searchTerm}`)
+    .then((response) => {
+      res.send(response);
+      console.log(response);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.log(error);
+    });
 });
-
-app.get('/api/tweets/random', (request, response) => {
+app.get('/api/tweets/random', async (req, res) => {
+  await getToken();
   const usernames = [
     'DalaiLama',
     'elonmusk',
@@ -27,65 +61,18 @@ app.get('/api/tweets/random', (request, response) => {
     'natfriedman',
     'peterthiel'
   ];
-
   const username = usernames[Math.floor(usernames.length * Math.random())];
-
-  console.log(getRandomTweets);
-
-  getRandomTweets(accessToken, username).then((tweets) => {
-    response.send(JSON.parse(tweets)[0]);
-  });
+  axios
+    .get(
+      `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${username}`
+    )
+    .then((response) => res.send(response.data[0]))
+    .catch((error) => {
+      res.sendStatus(500);
+      console.log(error);
+    });
 });
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react', 'build', 'index.html'));
-});
-
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+);
 app.listen(port, () => console.log(`Gator app listening on port ${port}`));
-
-request
-  .post(
-    'https://api.twitter.com/oauth2/token',
-    {
-      form: {
-        grant_type: 'client_credentials'
-      }
-    },
-    function (error, response, body) {
-      accessToken = JSON.parse(body).access_token;
-
-      if (accessToken) {
-      }
-    }
-  )
-  .auth(
-    'zb2Kc4J3yM7knCkY5ZQ7oqJhA',
-    'znNnMvwZrcpcIXSpNhNKSZfUN6KRr3EWYVehDOITfFmQi5BOms',
-    true
-  );
-
-function getTweets(accessToken, searchTerm) {
-  return request.get('https://api.twitter.com/1.1/search/tweets.json', {
-    auth: {
-      bearer: accessToken
-    },
-    qs: {
-      q: searchTerm
-    }
-  });
-}
-
-function getRandomTweets(accessToken, username) {
-  console.log('username: ', username);
-  return request.get(
-    'https://api.twitter.com/1.1/statuses/user_timeline.json',
-    {
-      auth: {
-        bearer: accessToken
-      },
-      qs: {
-        screen_name: username
-      }
-    }
-  );
-}
