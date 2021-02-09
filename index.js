1f0bccf109ff1ae5a96ee7044c54e679e19fbccf
Trevor Mearns <spiral272@gmail.com>
2021-02-09 17:47:25 +0900

    super confused :-(

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-require('dotenv').config;
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-const { response } = require('express');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 let accessToken = '';
 const getToken = () => {
   if (accessToken) return accessToken;
   const config = {
     headers: {
-      Content_Type: 'application/json'
+      Content_Type: 'application/x-www-form-urlencode'
     },
     auth: {
       username: process.env.API_KEY,
@@ -24,12 +23,14 @@ const getToken = () => {
       config
     )
     .then((response) => {
-      axios.defaults.headers.common = {
-        Authorization: `Bearer ${response.data.access_token}`
-      };
-      return response.data.access_token;
+      // axios.defaults.headers.common = {
+      //   Authorization: `Bearer ${response.data.access_token}`
+      // };
+      console.log('access token: ', response.data.access_token);
+      accessToken = response.data.access_token;
+      // return response.data.access_token;
     })
-    .catch((error) => console.log(error));
+    .catch((error) => console.log('error is ', error));
 };
 
 getToken();
@@ -39,12 +40,12 @@ app.use(
   express.static(path.join(__dirname, 'client', 'build', 'static'))
 );
 app.get('/api/tweets', async (req, res) => {
-  await getToken();
+  // await getToken();
   const searchTerm = req.query.searchTerm;
   axios
     .get(`https://api.twitter.com/1.1/search/tweets.json?q=${searchTerm}`)
     .then((response) => {
-      res.send(response);
+      res.send(JSON.parse(response));
       console.log(response);
     })
     .catch((error) => {
@@ -53,7 +54,7 @@ app.get('/api/tweets', async (req, res) => {
     });
 });
 app.get('/api/tweets/random', async (req, res) => {
-  await getToken();
+  // await getToken();
   const usernames = [
     'DalaiLama',
     'elonmusk',
@@ -64,8 +65,16 @@ app.get('/api/tweets/random', async (req, res) => {
   const username = usernames[Math.floor(usernames.length * Math.random())];
   axios
     .get(
-      `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${username}`
+      `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${username}`,
+      {
+        headers: {
+          Content-Type: 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${accessToken}`
+        }
+      }
     )
+    .then((response) => response.json())
     .then((response) => res.send(response.data[0]))
     .catch((error) => {
       res.sendStatus(500);
